96be80e5aa9ab4a63a4084793faae9466365c6b4
francisjosephironman <francis.3.6.joseph@gmail.com>
2021-11-19 17:58:51 +0800

    Added Tic Tac Toe functionality on the program. The first button click will show 'X' on that button, but the next button click will display 'O' on that button. Changed the color of the program.

--- TicTacToeProgram/TicTacToeProgram/Form1.cs
+++ TicTacToeProgram/TicTacToeProgram/Form1.cs
@@ -22,52 +22,135 @@ namespace TicTacToeProgram
 
         }
 
+        int flag = 1;
+
         Bitmap b = new Bitmap(@"C:\Users\franc\source\repos\Assign#5TicTacToe\TicTacToeProgram\TicTacToeProgram\Properties\RedX.PNG");
         Bitmap b1 = new Bitmap(@"C:\Users\franc\source\repos\Assign#5TicTacToe\TicTacToeProgram\TicTacToeProgram\Properties\BlueO.PNG");
 
         private void btnRow1Col1_Click(object sender, EventArgs e)
         {
-            btnRow1Col1.BackgroundImage = b;
+            if (flag == 1)
+            {
+                btnRow1Col1.BackgroundImage = b;
+                flag *= -1;
+            } else
+            {
+                btnRow1Col1.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow1Col2_Click(object sender, EventArgs e)
         {
-            btnRow1Col2.BackgroundImage = b1;
+            if (flag == 1)
+            {
+                btnRow1Col2.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow1Col2.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow1Col3_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow1Col3.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow1Col3.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow2Col1_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow2Col1.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow2Col1.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow2Col2_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow2Col2.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow2Col2.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow2Col3_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow2Col3.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow2Col3.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow3Col1_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow3Col1.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow3Col1.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow3Col2_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow3Col2.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow3Col2.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
 
         private void btnRow3Col3_Click(object sender, EventArgs e)
         {
-
+            if (flag == 1)
+            {
+                btnRow3Col3.BackgroundImage = b;
+                flag *= -1;
+            }
+            else
+            {
+                btnRow3Col3.BackgroundImage = b1;
+                flag *= -1;
+            }
         }
+
     }
 }
