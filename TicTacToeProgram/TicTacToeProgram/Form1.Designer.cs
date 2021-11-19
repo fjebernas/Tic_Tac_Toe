6b34c83992210e7ae55a7a06941c550fdd48bb96
francisjosephironman <francis.3.6.joseph@gmail.com>
2021-11-19 17:44:56 +0800

    Added X and O images to the program. Resized the program, made it bigger.

--- TicTacToeProgram/TicTacToeProgram/Form1.Designer.cs
+++ TicTacToeProgram/TicTacToeProgram/Form1.Designer.cs
@@ -42,92 +42,109 @@ namespace TicTacToeProgram
             // 
             // btnRow1Col1
             // 
+            this.btnRow1Col1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnRow1Col1.Location = new System.Drawing.Point(10, 10);
             this.btnRow1Col1.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow1Col1.Name = "btnRow1Col1";
-            this.btnRow1Col1.Size = new System.Drawing.Size(158, 157);
+            this.btnRow1Col1.Size = new System.Drawing.Size(211, 193);
             this.btnRow1Col1.TabIndex = 0;
             this.btnRow1Col1.UseVisualStyleBackColor = true;
             this.btnRow1Col1.Click += new System.EventHandler(this.btnRow1Col1_Click);
             // 
             // btnRow1Col2
             // 
-            this.btnRow1Col2.Location = new System.Drawing.Point(172, 10);
+            this.btnRow1Col2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow1Col2.Location = new System.Drawing.Point(235, 10);
             this.btnRow1Col2.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow1Col2.Name = "btnRow1Col2";
-            this.btnRow1Col2.Size = new System.Drawing.Size(158, 157);
+            this.btnRow1Col2.Size = new System.Drawing.Size(211, 193);
             this.btnRow1Col2.TabIndex = 1;
             this.btnRow1Col2.UseVisualStyleBackColor = true;
+            this.btnRow1Col2.Click += new System.EventHandler(this.btnRow1Col2_Click);
             // 
             // btnRow1Col3
             // 
-            this.btnRow1Col3.Location = new System.Drawing.Point(334, 10);
+            this.btnRow1Col3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow1Col3.Location = new System.Drawing.Point(460, 10);
             this.btnRow1Col3.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow1Col3.Name = "btnRow1Col3";
-            this.btnRow1Col3.Size = new System.Drawing.Size(158, 157);
+            this.btnRow1Col3.Size = new System.Drawing.Size(211, 193);
             this.btnRow1Col3.TabIndex = 2;
             this.btnRow1Col3.UseVisualStyleBackColor = true;
+            this.btnRow1Col3.Click += new System.EventHandler(this.btnRow1Col3_Click);
             // 
             // btnRow2Col1
             // 
-            this.btnRow2Col1.Location = new System.Drawing.Point(10, 171);
+            this.btnRow2Col1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow2Col1.Location = new System.Drawing.Point(10, 211);
             this.btnRow2Col1.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow2Col1.Name = "btnRow2Col1";
-            this.btnRow2Col1.Size = new System.Drawing.Size(158, 157);
+            this.btnRow2Col1.Size = new System.Drawing.Size(211, 193);
             this.btnRow2Col1.TabIndex = 0;
             this.btnRow2Col1.UseVisualStyleBackColor = true;
+            this.btnRow2Col1.Click += new System.EventHandler(this.btnRow2Col1_Click);
             // 
             // btnRow2Col2
             // 
-            this.btnRow2Col2.Location = new System.Drawing.Point(172, 171);
+            this.btnRow2Col2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow2Col2.Location = new System.Drawing.Point(235, 211);
             this.btnRow2Col2.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow2Col2.Name = "btnRow2Col2";
-            this.btnRow2Col2.Size = new System.Drawing.Size(158, 157);
+            this.btnRow2Col2.Size = new System.Drawing.Size(211, 193);
             this.btnRow2Col2.TabIndex = 1;
             this.btnRow2Col2.UseVisualStyleBackColor = true;
+            this.btnRow2Col2.Click += new System.EventHandler(this.btnRow2Col2_Click);
             // 
             // btnRow2Col3
             // 
-            this.btnRow2Col3.Location = new System.Drawing.Point(334, 171);
+            this.btnRow2Col3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow2Col3.Location = new System.Drawing.Point(460, 211);
             this.btnRow2Col3.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow2Col3.Name = "btnRow2Col3";
-            this.btnRow2Col3.Size = new System.Drawing.Size(158, 157);
+            this.btnRow2Col3.Size = new System.Drawing.Size(211, 193);
             this.btnRow2Col3.TabIndex = 2;
             this.btnRow2Col3.UseVisualStyleBackColor = true;
+            this.btnRow2Col3.Click += new System.EventHandler(this.btnRow2Col3_Click);
             // 
             // btnRow3Col1
             // 
-            this.btnRow3Col1.Location = new System.Drawing.Point(10, 333);
+            this.btnRow3Col1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow3Col1.Location = new System.Drawing.Point(10, 418);
             this.btnRow3Col1.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow3Col1.Name = "btnRow3Col1";
-            this.btnRow3Col1.Size = new System.Drawing.Size(158, 157);
+            this.btnRow3Col1.Size = new System.Drawing.Size(211, 193);
             this.btnRow3Col1.TabIndex = 0;
             this.btnRow3Col1.UseVisualStyleBackColor = true;
+            this.btnRow3Col1.Click += new System.EventHandler(this.btnRow3Col1_Click);
             // 
             // btnRow3Col2
             // 
-            this.btnRow3Col2.Location = new System.Drawing.Point(172, 333);
+            this.btnRow3Col2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow3Col2.Location = new System.Drawing.Point(235, 418);
             this.btnRow3Col2.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow3Col2.Name = "btnRow3Col2";
-            this.btnRow3Col2.Size = new System.Drawing.Size(158, 157);
+            this.btnRow3Col2.Size = new System.Drawing.Size(211, 193);
             this.btnRow3Col2.TabIndex = 1;
             this.btnRow3Col2.UseVisualStyleBackColor = true;
+            this.btnRow3Col2.Click += new System.EventHandler(this.btnRow3Col2_Click);
             // 
             // btnRow3Col3
             // 
-            this.btnRow3Col3.Location = new System.Drawing.Point(334, 333);
+            this.btnRow3Col3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRow3Col3.Location = new System.Drawing.Point(460, 418);
             this.btnRow3Col3.Margin = new System.Windows.Forms.Padding(2);
             this.btnRow3Col3.Name = "btnRow3Col3";
-            this.btnRow3Col3.Size = new System.Drawing.Size(158, 157);
+            this.btnRow3Col3.Size = new System.Drawing.Size(211, 193);
             this.btnRow3Col3.TabIndex = 2;
             this.btnRow3Col3.UseVisualStyleBackColor = true;
+            this.btnRow3Col3.Click += new System.EventHandler(this.btnRow3Col3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(502, 497);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(682, 624);
             this.Controls.Add(this.btnRow3Col3);
             this.Controls.Add(this.btnRow3Col2);
             this.Controls.Add(this.btnRow2Col3);
--- TicTacToeProgram/TicTacToeProgram/Form1.cs
+++ TicTacToeProgram/TicTacToeProgram/Form1.cs
@@ -22,12 +22,50 @@ namespace TicTacToeProgram
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        Bitmap b = new Bitmap(@"C:\Users\franc\source\repos\Assign#5TicTacToe\TicTacToeProgram\TicTacToeProgram\Properties\RedX.PNG");
+        Bitmap b1 = new Bitmap(@"C:\Users\franc\source\repos\Assign#5TicTacToe\TicTacToeProgram\TicTacToeProgram\Properties\BlueO.PNG");
+
+        private void btnRow1Col1_Click(object sender, EventArgs e)
+        {
+            btnRow1Col1.BackgroundImage = b;
+        }
+
+        private void btnRow1Col2_Click(object sender, EventArgs e)
+        {
+            btnRow1Col2.BackgroundImage = b1;
+        }
+
+        private void btnRow1Col3_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void btnRow1Col1_Click(object sender, EventArgs e)
+        private void btnRow2Col1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRow2Col2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRow2Col3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRow3Col1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRow3Col2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRow3Col3_Click(object sender, EventArgs e)
         {
 
         }
