655d26bcdb3c746c6e1d72b3a47c4a3aeb99fd28
francisjosephironman <francis.3.6.joseph@gmail.com>
2021-11-21 20:54:57 +0800

    Added a Check() function that checks if there is a three consecutive X or O in the grid, be it a horizontal, vertical or diagonal. If there is one, the displayBox will announce the winner.

--- TicTacToeProgram/TicTacToeProgram/Form1.cs
+++ TicTacToeProgram/TicTacToeProgram/Form1.cs
@@ -23,6 +23,12 @@ namespace TicTacToeProgram
         }
 
         int flag = 1;
+        int[] matrix =
+        {
+            0,0,0,
+            0,0,0,
+            0,0,0
+        };
 
         Bitmap b = new Bitmap(@"C:\Users\franc\source\repos\Assign#5TicTacToe\TicTacToeProgram\TicTacToeProgram\Properties\RedX.PNG");
         Bitmap b1 = new Bitmap(@"C:\Users\franc\source\repos\Assign#5TicTacToe\TicTacToeProgram\TicTacToeProgram\Properties\BlueO.PNG");
@@ -32,12 +38,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow1Col1.BackgroundImage = b;
+                matrix[0] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
+                
             } else
             {
                 btnRow1Col1.BackgroundImage = b1;
+                matrix[0] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -49,13 +60,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow1Col2.BackgroundImage = b;
+                matrix[1] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow1Col2.BackgroundImage = b1;
+                matrix[1] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -67,13 +82,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow1Col3.BackgroundImage = b;
+                matrix[2] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow1Col3.BackgroundImage = b1;
+                matrix[2] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -85,13 +104,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow2Col1.BackgroundImage = b;
+                matrix[3] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow2Col1.BackgroundImage = b1;
+                matrix[3] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -103,13 +126,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow2Col2.BackgroundImage = b;
+                matrix[4] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow2Col2.BackgroundImage = b1;
+                matrix[4] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -121,13 +148,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow2Col3.BackgroundImage = b;
+                matrix[5] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow2Col3.BackgroundImage = b1;
+                matrix[5] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -139,13 +170,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow3Col1.BackgroundImage = b;
+                matrix[6] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow3Col1.BackgroundImage = b1;
+                matrix[6] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -157,13 +192,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow3Col2.BackgroundImage = b;
+                matrix[7] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow3Col2.BackgroundImage = b1;
+                matrix[7] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -175,13 +214,17 @@ namespace TicTacToeProgram
             if (flag == 1)
             {
                 btnRow3Col3.BackgroundImage = b;
+                matrix[8] = 1;
                 OTurn();
+                Check();
                 flag *= -1;
             }
             else
             {
                 btnRow3Col3.BackgroundImage = b1;
+                matrix[8] = 2;
                 XTurn();
+                Check();
                 flag *= -1;
             }
 
@@ -196,6 +239,41 @@ namespace TicTacToeProgram
         private void OTurn()
         {
             displayBox.Text = "Your Turn O!";
+        }
+
+        private void Check()
+        {
+            if (matrix[0] == 1 && matrix[1] == 1 && matrix[2] == 1 ||
+                matrix[3] == 1 && matrix[4] == 1 && matrix[5] == 1 ||
+                matrix[6] == 1 && matrix[7] == 1 && matrix[8] == 1 ||
+
+                matrix[0] == 1 && matrix[3] == 1 && matrix[6] == 1 ||
+                matrix[1] == 1 && matrix[4] == 1 && matrix[7] == 1 ||
+                matrix[2] == 1 && matrix[5] == 1 && matrix[8] == 1 ||
+
+                matrix[0] == 1 && matrix[4] == 1 && matrix[8] == 1 ||
+                matrix[2] == 1 && matrix[4] == 1 && matrix[6] == 1
+                )
+            {
+                displayBox.Text = "X WINS!";
+                
+            }
+            else if (
+              matrix[0] == 2 && matrix[1] == 2 && matrix[2] == 2 ||
+              matrix[3] == 2 && matrix[4] == 2 && matrix[5] == 2 ||
+              matrix[6] == 2 && matrix[7] == 2 && matrix[8] == 2 ||
+
+              matrix[0] == 2 && matrix[3] == 2 && matrix[6] == 2 ||
+              matrix[1] == 2 && matrix[4] == 2 && matrix[7] == 2 ||
+              matrix[2] == 2 && matrix[5] == 2 && matrix[8] == 2 ||
+
+              matrix[0] == 2 && matrix[4] == 2 && matrix[8] == 2 ||
+              matrix[2] == 2 && matrix[4] == 2 && matrix[6] == 2
+              )
+            {
+                displayBox.Text = "O WINS!";
+                
+            }
         }
     }
 }
