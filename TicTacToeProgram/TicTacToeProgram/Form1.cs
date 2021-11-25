86ad4bf5c40a37cb101f0a17eb58dcd6c3b12863
francisjosephironman <francis.3.6.joseph@gmail.com>
2021-11-25 20:46:56 +0800

    Added a functionality that asks the user through a message box for confirmation if they pressed the 'x'(close) button.

--- TicTacToeProgram/TicTacToeProgram/Form1.cs
+++ TicTacToeProgram/TicTacToeProgram/Form1.cs
@@ -562,5 +562,25 @@ namespace TicTacToeProgram
         {
             Application.Restart();
         }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (PreClosingConfirmation() == System.Windows.Forms.DialogResult.Yes)
+            {
+                Dispose(true);
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private DialogResult PreClosingConfirmation()
+        {
+            DialogResult res = System.Windows.Forms.MessageBox.Show(" Do you want to quit?          ", "Quit Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return res;
+        }
     }
 }
